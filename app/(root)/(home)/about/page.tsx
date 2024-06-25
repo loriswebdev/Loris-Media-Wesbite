009c256cc3526789a0f019cc5c2540a128cdc372
Robert <Robmel@Roberts-MacBook-Pro.local>
2024-06-25 13:47:21 -0600

    layout updates

--- app/(root)/(home)/about/page.tsx
+++ app/(root)/(home)/about/page.tsx
@@ -11,7 +11,7 @@ export default function About() {
   return (
     <>
     <div className="md:pt-[100px]">
-    <ProjectCard className={`bg-gradient-purple-dark pt-16 pl-16 pb-16  pr-16  lg:pr-8 text-white md:flex-row flex-col flex gap-[20px] items-center justify-center`} content="Hi I'm Robert Melnyk. Founder and creator of Loris Media. I've been working as a freelance developer for 3 years. I started my career in tech because I wanted to make mine and other's dreams a reality. I love new ideas, I love to dream up new concepts and see them take form in the real world. It's not always rosey, but I even love the growing pains and deep learning that comes along with a truley novel project. I hope you enjoy the current work shown here, and I look forward to the opportunity to work with you."  title='About The Creator'>
+    <ProjectCard className={`bg-gradient-purple-dark pt-16 pl-16 pb-16  pr-16  lg:pr-8 text-white md:flex-row flex-col flex gap-[20px] items-center justify-center`} content="Hi, I'm Robert Melnyk, the founder and creator of Loris Media. I have been working as a freelance developer for 3 years. I started my career in tech because I wanted to make my dreams and others' dreams a reality. I love new ideas, dreaming up new concepts, and seeing them take form in the real world. It's not always rosy, but I even love the growing pains and deep learning that come along with a truly novel project. I hope you enjoy the current work shown here, and I look forward to the opportunity to work with you."  title='About The Creator'>
     <ExampleCard  imgUrl='/images/robYclau.jpg' className="bg-cover min-w-[300px] h-[350px] w-[300] lg:w-[400px] float-right lg:h-[450px] card-shadow bg-white rounded-lg" />
   </ProjectCard>
     </div>
